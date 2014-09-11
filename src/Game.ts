8364e96d53496345d0cb34d724b8cb7d7e4d52f8
Храмков Евгений <ewgeniux@gmail.com>
2014-09-11 15:53:49 +0400

    three.js add

--- src/Game.ts
+++ src/Game.ts
@@ -1,21 +1,13 @@
 /**
- * Created by ewgenius on 23.08.14.
+ * Created by ewgenius on 11.09.14.
  */
-/// <reference path="../bower_components/phaser/build/phaser.d.ts" />
-module Amputator {
-    export class Game extends Phaser.Game {
+/// <reference path="../lib/three.d.ts" />
+module Xube {
+    export class Game {
         constructor() {
-            super(800, 600, Phaser.AUTO, 'content', null);
-
-            this.state.add('Boot', Boot, false);
-
-            this.state.add('Preloader', Preloader, false);
-
-            this.state.start('Boot');
         }
     }
 }
 
 window.onload = () => {
-    var game = new Amputator.Game();
 };
