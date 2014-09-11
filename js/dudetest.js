87fa3f519b391b6739a492caa47f910e65901fe7
Храмков Евгений <ewgeniux@gmail.com>
2014-09-11 18:01:16 +0400

    simple model test

--- js/dudetest.js
+++ js/dudetest.js
@@ -72,9 +72,26 @@ var DudeTest;
             _super.apply(this, arguments);
         }
         DudeGame.prototype.initialize = function () {
-            console.log('initialized');
+            var _this = this;
             _super.prototype.initialize.call(this);
+
             this.renderer.setSize(800, 600);
+
+            this.camera = new THREE.PerspectiveCamera(45, 800 / 600, 1, 2000);
+            this.camera.position.set(0, 200, 800);
+
+            (function () {
+                _this.scene.add(new THREE.AmbientLight(0x404040));
+
+                var light = new THREE.DirectionalLight(0xffffff);
+                light.position.set(0, 1, 0);
+                _this.scene.add(light);
+
+                var geometry = new THREE.BoxGeometry(200, 200, 200);
+                var material = new THREE.MeshBasicMaterial();
+                var mesh = new THREE.Mesh(geometry, material);
+                _this.scene.add(mesh);
+            })();
         };
         return DudeGame;
     })(Xube.Game);
--- src/dudetest/DudeTest.ts
+++ src/dudetest/DudeTest.ts
@@ -3,12 +3,31 @@
  */
 /// <reference path="../xube/Game.ts" />
 /// <reference path="../xube/GameObject.ts" />
+
+/// <reference path="../../lib/three.d.ts" />
 module DudeTest {
     export class DudeGame extends Xube.Game {
         initialize() {
-            console.log('initialized');
             super.initialize();
+
             this.renderer.setSize(800, 600);
+
+            this.camera = new THREE.PerspectiveCamera(45, 800 / 600, 1, 2000);
+            this.camera.position.set(0, 200, 800);
+
+            // init scene
+            (() => {
+                this.scene.add(new THREE.AmbientLight(0x404040));
+
+                var light = new THREE.DirectionalLight(0xffffff);
+                light.position.set(0, 1, 0);
+                this.scene.add(light);
+
+                var geometry = new THREE.BoxGeometry( 200, 200, 200 );
+                var material = new THREE.MeshBasicMaterial();
+                var mesh = new THREE.Mesh( geometry, material );
+                this.scene.add( mesh );
+            })();
         }
     }
 }
